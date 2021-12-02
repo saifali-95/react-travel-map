105479b67892001fdb4a27ec2bd34543a8cd7218
Saif Ali <saifali7646@gmail.com>
2021-12-02 10:11:58 -0500

    setting up user login and registration routes and database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const dotevn = require("dotenv")
+const dotenv = require("dotenv")
+const markerRoute = require("./routes/marker");
+const usersRoute = require("./routes/users");
 
 const app = express();
-dotevn.config();
+dotenv.config();
 
 app.use(express.json())
 
@@ -16,6 +18,10 @@ mongoose
         console.log("MongoDB Connected!")
     })
     .catch((error) => console.log(error));
+
+
+app.use("/api/marker", markerRoute);
+app.use("/api/users", usersRoute);
 
 app.listen(8800,()=>{
     console.log("Backend server is running")
