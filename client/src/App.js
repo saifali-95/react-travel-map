0c5dbd9a277427ee6ffda76e2cc27e060e8d104c
Saif Ali <saifali7646@gmail.com>
2021-12-07 11:06:31 -0500

    use Array.fill to render StarIcon component

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import "./App.css";
 import { format } from "timeago.js";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("John"); 
   const [markers, setMarkers] = useState([]);
+  const [newPlace, setNewPlace] = useState(null);
   const [popUpId, setPopUpId] = useState(null);
   const [viewport, setViewport] = useState({
     width: "100vw",
@@ -15,11 +17,19 @@ function App() {
     latitude: 31.7917,
     longitude: 7.0926,
     zoom: 2,
-    markerSize: 20,
+    markerSize: 10,
   });
 
   const showPopUp = function(id) {
     setPopUpId(id);
+  }
+
+  const handleAddClick = (e)=> {
+    const [long, lat] = e.lngLat;
+    setNewPlace({
+      long,
+      lat
+    })
   }
 
   useEffect(() => {
@@ -40,6 +50,7 @@ function App() {
       mapboxApiAccessToken={`${process.env.REACT_APP_MAPBOX}`}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
       mapStyle="mapbox://styles/saifali95/ckvgcknrl07s314mlguc7yojt"
+      onDblClick = {handleAddClick}
     >
       {markers.map((p) => (
         <>
@@ -51,8 +62,9 @@ function App() {
           >
             <RoomIcon
               style={{
-                fontSize: viewport.markerSize * viewport.zoom,
-                color: "slateblue",
+                fontSize: 12 * viewport.zoom,
+                color: p.username === currentUser ? "tomato":"slateblue",
+                cursor : "pointer"
               }}
               onClick = {()=> showPopUp(p._id)}
             />
@@ -75,18 +87,27 @@ function App() {
               <p>{p.desc}</p>
               <label>Rating</label>
               <div className="stars">
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
+                {Array(p.rating).fill(<StarIcon className="star"/>)}
               </div>
               <label>Information</label>
               <span className="username">
-                Created by <b>{p.username}</b>
+                Created by <b>{p.username === currentUser ? "you" : p.username}</b>
               </span>
               <span className="date">{format(p.createdAt)}</span>
             </div>
+          </Popup>}
+          {newPlace &&
+          <Popup
+            latitude={newPlace.lat}
+            longitude={newPlace.long}
+            closeButton={true}
+            closeOnClick={false}
+            offsetLeft={20}
+            offsetTop={10}
+            anchor="left"
+            onClose={()=> setNewPlace(null)}
+          >
+            Hello
           </Popup>}
         </>
       ))}
