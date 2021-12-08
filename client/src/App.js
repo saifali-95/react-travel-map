5acefedf16d6b54b74dacd1c8237865296849406
Saif Ali <saifali7646@gmail.com>
2021-12-08 12:44:50 -0500

    add login, logout, register button

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 import { format } from "timeago.js";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState("John"); 
+  const [currentUser, setCurrentUser] = useState("Ali");
   const [markers, setMarkers] = useState([]);
   const [newPlace, setNewPlace] = useState(null);
   const [popUpId, setPopUpId] = useState(null);
@@ -23,17 +23,17 @@ function App() {
     markerSize: 10,
   });
 
-  const showPopUp = function(id) {
+  const showPopUp = function (id) {
     setPopUpId(id);
-  }
+  };
 
-  const handleAddClick = (e)=> {
+  const handleAddClick = (e) => {
     const [long, lat] = e.lngLat;
     setNewPlace({
       long,
-      lat
-    })
-  }
+      lat,
+    });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -73,107 +73,117 @@ function App() {
       mapboxApiAccessToken={`${process.env.REACT_APP_MAPBOX}`}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
       mapStyle="mapbox://styles/saifali95/ckvgcknrl07s314mlguc7yojt"
-      onDblClick = {handleAddClick}
+      onDblClick={handleAddClick}
     >
       {markers.map((p) => (
         <>
           <Marker
             latitude={p.lat}
             longitude={p.long}
-            offsetLeft={-viewport.zoom*5}
-            offsetTop={-viewport.zoom*10}
+            offsetLeft={-viewport.zoom * 5}
+            offsetTop={-viewport.zoom * 10}
           >
             <RoomIcon
               style={{
                 fontSize: 12 * viewport.zoom,
-                color: p.username === currentUser ? "tomato":"slateblue",
-                cursor : "pointer"
+                color: p.username === currentUser ? "tomato" : "slateblue",
+                cursor: "pointer",
               }}
-              onClick = {()=> showPopUp(p._id)}
+              onClick={() => showPopUp(p._id)}
             />
           </Marker>
-          {popUpId === p._id &&
-          <Popup
-            latitude={p.lat}
-            longitude={p.long}
-            closeButton={true}
-            closeOnClick={false}
-            offsetLeft={20}
-            offsetTop={10}
-            anchor="left"
-            onClose={()=> setPopUpId(null)}
-          >
-            <div className="card">
-              <label>Place</label>
-              <h4 className="place">{p.title}</h4>
-              <label>Review</label>
-              <p>{p.desc}</p>
-              <label>Rating</label>
-              <div className="stars">
-                {Array(p.rating).fill(<StarIcon className="star"/>)}
-              </div>
-              <label>Information</label>
-              <span className="username">
-                Created by <b>{p.username === currentUser ? "you" : p.username}</b>
-              </span>
-              <span className="date">{format(p.createdAt)}</span>
-            </div>
-          </Popup>}
-          {newPlace && (
-          <>
-            <Marker
-              latitude={newPlace.lat}
-              longitude={newPlace.long}
-              offsetLeft={-3.5 * viewport.zoom}
-              offsetTop={-7 * viewport.zoom}
-            >
-              <RoomIcon
-                style={{
-                  fontSize: 7 * viewport.zoom,
-                  color: "tomato",
-                  cursor: "pointer",
-                }}
-              />
-            </Marker>
+          {popUpId === p._id && (
             <Popup
-              latitude={newPlace.lat}
-              longitude={newPlace.long}
+              latitude={p.lat}
+              longitude={p.long}
               closeButton={true}
               closeOnClick={false}
-              onClose={() => setNewPlace(null)}
+              offsetLeft={20}
+              offsetTop={10}
               anchor="left"
+              onClose={() => setPopUpId(null)}
             >
-              <div>
-                <form onSubmit={handleSubmit}>
-                  <label>Title</label>
-                  <input
-                    placeholder="Enter a title"
-                    autoFocus
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                  <label>Description</label>
-                  <textarea
-                    placeholder="Say us something about this place."
-                    onChange={(e) => setDesc(e.target.value)}
-                  />
-                  <label>Rating</label>
-                  <select onChange={(e) => setStar(e.target.value)}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                  </select>
-                  <button type="submit" className="submitButton">
-                    Add Pin
-                  </button>
-                </form>
+              <div className="card">
+                <label>Place</label>
+                <h4 className="place">{p.title}</h4>
+                <label>Review</label>
+                <p>{p.desc}</p>
+                <label>Rating</label>
+                <div className="stars">
+                  {Array(p.rating).fill(<StarIcon className="star" />)}
+                </div>
+                <label>Information</label>
+                <span className="username">
+                  Created by{" "}
+                  <b>{p.username === currentUser ? "you" : p.username}</b>
+                </span>
+                <span className="date">{format(p.createdAt)}</span>
               </div>
             </Popup>
-          </>
-        )}
+          )}
+          {newPlace && (
+            <>
+              <Marker
+                latitude={newPlace.lat}
+                longitude={newPlace.long}
+                offsetLeft={-3.5 * viewport.zoom}
+                offsetTop={-7 * viewport.zoom}
+              >
+                <RoomIcon
+                  style={{
+                    fontSize: 7 * viewport.zoom,
+                    color: "tomato",
+                    cursor: "pointer",
+                  }}
+                />
+              </Marker>
+              <Popup
+                latitude={newPlace.lat}
+                longitude={newPlace.long}
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setNewPlace(null)}
+                anchor="left"
+              >
+                <div>
+                  <form onSubmit={handleSubmit}>
+                    <label>Title</label>
+                    <input
+                      placeholder="Enter a title"
+                      autoFocus
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <label>Description</label>
+                    <textarea
+                      placeholder="Say us something about this place."
+                      onChange={(e) => setDesc(e.target.value)}
+                    />
+                    <label>Rating</label>
+                    <select onChange={(e) => setStar(e.target.value)}>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+                    <button type="submit" className="submitButton">
+                      Add Pin
+                    </button>
+                  </form>
+                </div>
+              </Popup>
+            </>
+          )}
         </>
       ))}
+      {currentUser ? (
+        <button className="button logout">Logout</button>
+      ) : (
+        <div className="button">
+          <button className="button login">Login</button>
+          <button className="button register">Register</button>
+        </div>
+      )}
     </ReactMapGL>
   );
 }
